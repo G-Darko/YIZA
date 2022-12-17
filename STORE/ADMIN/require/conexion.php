38fa3ceb0c00fb6f0b747e011a5a2b8abef07f3b
Gael Uribe <112705344+G-Darko@users.noreply.github.com>
2022-12-16 19:24:27 -0600

    Update conexion.php

--- STORE/ADMIN/require/conexion.php
+++ STORE/ADMIN/require/conexion.php
@@ -1,9 +1,14 @@
 <?php  
 	//crear variables conexion
-	$servername="sql5.freesqldatabase.com";
-	$bd="sql5516956";
-	$username="sql5516956";
-	$password="jBXmc7yHJ8";
+	//$servername="sql5.freesqldatabase.com";
+	//$bd="sql5516956";
+	//$username="sql5516956";
+	//$password="jBXmc7yHJ8";
+	
+	$servername="sql304.epizy.com";
+	$bd="epiz_33215016_yiza";
+	$username="epiz_33215016";
+	$password="hEzLy5XbpT3uL7";
 
 	$response = new stdClass();
 	error_reporting(0); //ocultar o mostrar los errores 
